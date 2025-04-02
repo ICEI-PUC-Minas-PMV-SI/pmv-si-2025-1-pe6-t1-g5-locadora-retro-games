9545b15da6d838d1fcd7cc68228b655b5ebbc1fc
matheusacad <114116491+matheusacad@users.noreply.github.com>
2025-04-01 21:04:21 -0300

    Update user.controller.js

--- src/backend/src/modules/user/user.controller.js
+++ src/backend/src/modules/user/user.controller.js
@@ -29,23 +29,6 @@ UserController.insertUser = async (req, res) => {
     res.status(500).json("Internal server error");
   }
 };
-UserController.updateUser = async (req, res) => {
-  try {
-    const body = {
-      id: Number(req.params.id),
-      name: req.body.name,
-      email: req.body.email,
-      cpf: req.body.cpf,
-      password: req.body.password,
-    };
-    console.log(req.body);
-    await UserService.update(body);
-    res.status(200).json("User updated successfully");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json("Internal server error");
-  }
-};
 UserController.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
