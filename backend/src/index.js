40abab898b13cc53719e1dabd0a84faee348793f
Michael <michaelhenriqued@gmail.com>
2025-04-06 20:26:21 -0300

    Add prod environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from 'body-parser'
 import swaggerUi from 'swagger-ui-express'
-import swaggerFile from './config/swagger-output.json' assert { type: "json" };
+import swaggerFile from './config/swagger-output.json' with { type: "json" };
 import dotenv from 'dotenv';
+import path from 'path'
 
 import AuthRouter from "./modules/auth/auth.route.js";
 import UserRouter from "./modules/user/user.route.js";
@@ -10,7 +11,9 @@ import GameRouter from "./modules/game/game.route.js";
 import ConsoleRouter from "./modules/console/console.route.js";
 import OrderRouter from "./modules/order/order.route.js";
 
-dotenv.config();
+const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env';
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
